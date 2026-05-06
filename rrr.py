f04c6e1281aa6434ef9a7f81a97d9e65f6ac1f35
mikhainevzorov <mikha1l.a.nevzorov@yandex.ru>
2026-05-06 16:59:38 +0300

    Update rrr.py

--- rrr.py
+++ rrr.py
@@ -1,9 +1,36 @@
-print('В каком году закончилась Вторая мировая война?')
-print('1 - 1995, 2 - 1925, 3 - 1945, 4 - 2005')
-answer = input('Напишите номер правильного ответа:')
+from pygame import * 
 
-if answer == '3':
-    print('Абсолютно верно!')
-else:
-    print('Неверно. В 1945')
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),  wight, height)
+        self.speed = player_speed 
+        self.rect = self.image.get_rect
+        self.rect.x =  plater_x
+        self.rect.y = plater_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys [K_UP] and self.rect.y > 5:
+            self.rect.y == self.speed 
+        if keys [K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys [K_w] and self.rect.y > 5:
+            self.rect.y == self.speed 
+        if keys [K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed 
+
+    back = (200, 255, 255)
+    win_width = 600
+    win_heidht = 500
+    window = display.set_mode win_
+        
+    
 
